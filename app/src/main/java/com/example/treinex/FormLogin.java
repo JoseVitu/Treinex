a15be0b20bb3eed7b3583785c60fd51179696a5e
JoseVitu <jose.sito@edu.unipar.br>
2023-04-02 22:50:36 -0300

    Adicionado método para botão de deslogar, e AlertDialog para redefinir senha (não está funcional ainda a parte de redefinição)

--- app/src/main/java/com/example/treinex/FormLogin.java
+++ app/src/main/java/com/example/treinex/FormLogin.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -11,15 +12,20 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthSettings;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.ktx.Firebase;
 
 public class FormLogin extends AppCompatActivity {
@@ -38,10 +44,7 @@ public class FormLogin extends AppCompatActivity {
         forgot_pass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder esqueceuSenha = new AlertDialog.Builder(FormLogin.this);
-                esqueceuSenha.setMessage("Entre em contato com jose.sito@cvale.com.br para alterar sua senha");
-                esqueceuSenha.setTitle("Treinex");
-                esqueceuSenha.create().show();
+                alertRedefinirSenha();
             }
         });
         bt_entrar.setOnClickListener(new View.OnClickListener() {
@@ -51,19 +54,20 @@ public class FormLogin extends AppCompatActivity {
                 String email = campo_login.getText().toString();
                 String senha = campo_senha.getText().toString();
 
+
                 if (email.isEmpty() || senha.isEmpty()){
                     Snackbar snackbar = Snackbar.make(view, mensagens[0],Snackbar.LENGTH_SHORT);
                     snackbar.setBackgroundTint(Color.WHITE);
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
                 }else{
-                    AutenticarUsuario();
+                    AutenticarUsuario(view);
                 }
 
             }
         });
     }
-    private void AutenticarUsuario(){
+    private void AutenticarUsuario(View view){
         String email = campo_login.getText().toString();
         String senha = campo_senha.getText().toString();
 
@@ -81,15 +85,86 @@ public class FormLogin extends AppCompatActivity {
                         }
                     }, 3000);
 
+                }else{
+
+                    String erro;
+
+                    try {
+
+                        throw task.getException();
+
+                    }catch(Exception e){
+                        erro = "E-mail ou senha incorretos! Certifique-se de estar digitando corretamente.";
+
+                    }
+                    Snackbar snackbar = Snackbar.make(view, erro,Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
                 }
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(usuarioAtual != null){
+            TelaPrincipal();
+        }
+
+    }
+
     private void TelaPrincipal(){
         Intent intent = new Intent(FormLogin.this, TelaPrincipal.class);
         startActivity(intent);
         finish();
+
     }
+
+
+    //problema nao solucionado
+    private void alertRedefinirSenha(){
+
+            AlertDialog.Builder redefinirSenha = new AlertDialog.Builder(this);
+            redefinirSenha.setTitle("Redefinir senha");
+            redefinirSenha.setMessage("Insira seu endereço de e-mail: ");
+            redefinirSenha.setCancelable(false);
+            EditText campoDigitarEmail = new EditText(this);
+            LinearLayout.LayoutParams linearLayout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            redefinirSenha.setView(campoDigitarEmail);
+            campoDigitarEmail.setLayoutParams(linearLayout);
+            redefinirSenha.setNegativeButton("Cancelar", null);
+            redefinirSenha.setPositiveButton("Recuperar senha", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    FirebaseAuth.getInstance().sendPasswordResetEmail(campoDigitarEmail.getText().toString().trim()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+
+                            Toast.makeText(getBaseContext(), "E-mail enviado", Toast.LENGTH_LONG).show();
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getBaseContext(), "E-mail errado", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            });
+            redefinirSenha.create().show();
+
+
+    }
+
+
+
+
+
     private void iniciarComponentes() {
         forgot_pass = findViewById(R.id.forgot_pass);
         campo_login = findViewById(R.id.campo_login);
@@ -97,4 +172,6 @@ public class FormLogin extends AppCompatActivity {
         bt_entrar = findViewById(R.id.bt_entrar);
         progress_bar = findViewById(R.id.progress_bar);
     }
+
+
 }
--- app/src/main/java/com/example/treinex/TelaPrincipal.java
+++ app/src/main/java/com/example/treinex/TelaPrincipal.java
@@ -2,13 +2,40 @@ package com.example.treinex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TelaPrincipal extends AppCompatActivity {
-
+    private Button bt_deslogar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_tela_principal);
+        iniciarComponentes();
+
+        bt_deslogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                formLogin();
+            }
+        });
+
+
+    }
+    private void formLogin(){
+        Intent intent = new Intent(TelaPrincipal.this, FormLogin.class);
+        startActivity(intent);
+        finish();
+
+    }
+    private void iniciarComponentes(){
+        bt_deslogar = findViewById(R.id.botao_deslogar);
+
     }
 }
