470a92cd4ddf3880460e4b241d3a4fc7e6da340b
josevitu <jose.sito@edu.unipar.br>
2023-04-11 22:06:03 -0300

    alterações na tela principal e criãção da tela de criação de trein.

---
+++ app/src/main/java/com/example/treinex/TelaCriarTurma.java
@@ -0,0 +1,14 @@
+package com.example.treinex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaCriarTurma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_criar_turma);
+    }
+}
